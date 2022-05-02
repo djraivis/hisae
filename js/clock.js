ac9da6e568e4bb6cd3eea6eef9422385ab6ad1be
Raivis <highdoven@gmail.com>
2022-05-02 20:37:20 +0100

    2022 Updates 2

--- js/clock.js
+++ js/clock.js
@@ -1,76 +1,76 @@
-(function(window, document, $) {
-  $(function(){
-    init();
-  });
+(function (window, document, $) {
+  $(function () {
+    init()
+  })
 
-  function init() {
-    var clock = new Clock();
-    clock.startClock();
+  function init () {
+    var clock = new Clock()
+    clock.startClock()
   }
 
-  function Clock() {
-    this.time = '';
+  function Clock () {
+    this.time = ''
   }
   Clock.prototype = {
-    startClock: function() {
-      var scope = this;
-      setInterval(this.updateClock.bind(this, scope), 500);
+    startClock: function () {
+      var scope = this
+      setInterval(this.updateClock.bind(this, scope), 500)
     },
-    updateClock: function(scope) {
-      var strArr = scope.getTime();
-      this.updateClockView(strArr);
+    updateClock: function (scope) {
+      var strArr = scope.getTime()
+      this.updateClockView(strArr)
 
     },
-    updateClockView: function(strArr) {
-      var targets = $('.clock-container>ul>li>span');
-      for(var i = 0; i < 3; i++) {
-        targets.eq(i).html(strArr[i]);
+    updateClockView: function (strArr) {
+      var targets = $('.clock-container>ul>li>span')
+      for (var i = 0; i < 3; i++) {
+        targets.eq(i).html(strArr[i])
       }
     },
-    getTime: function() {
-      var today=new Date();
-      var h=today.getUTCHours();
-      var m=today.getUTCMinutes();
-      var s=today.getUTCSeconds();
+    getTime: function () {
+      var today = new Date()
+      var h = today.getUTCHours()
+      var m = today.getUTCMinutes()
+      var s = today.getUTCSeconds()
 
-      var hoursArr = this.convertHourByTimeZone(h);
+      var hoursArr = this.convertHourByTimeZone(h)
 
-      m = this.fixLayout(m);
-      s = this.fixLayout(s);
+      m = this.fixLayout(m)
+      s = this.fixLayout(s)
 
-      var strArr = [];
-      for(var i = 0; i < 3; i++) {
-        var str = hoursArr[i] + ':' + m + ':' + s;
-        strArr.push(str);
+      var strArr = []
+      for (var i = 0; i < 3; i++) {
+        var str = hoursArr[i] + ':' + m + ':' + s
+        strArr.push(str)
       }
-      return strArr;
+      return strArr
     },
-    convertHourByTimeZone: function(h) {
+    convertHourByTimeZone: function (h) {
       // order Tokyo, Riga, London
-      var arr = [9, 3, 1,];
-      var hourArr = [];
+      var arr = [9, 3, 1,]
+      var hourArr = []
 
-      for(var i = 0; i < 3; i++) {
-        var hour = h + arr[i];
-        if(24 <= hour) {
-          hour = hour - 24;
-        } else if(hour < 0) {
-          hour = 24 + hour;
+      for (var i = 0; i < 3; i++) {
+        var hour = h + arr[i]
+        if (24 <= hour) {
+          hour = hour - 24
+        } else if (hour < 0) {
+          hour = 24 + hour
         }
 
         // push
-        hourArr.push(hour);
+        hourArr.push(hour)
 
       }
 
-      return hourArr;
+      return hourArr
     },
-    fixLayout: function(i) {
-      if (i<10) {
-        i = "0" + i;
+    fixLayout: function (i) {
+      if (i < 10) {
+        i = "0" + i
       }  // add zero in front of numbers < 10
-      return i;
+      return i
     }
-  };
+  }
 
-})(window, document, window.jQuery);
+})(window, document, window.jQuery)
